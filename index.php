8ffff6f761c8eadae1576f06e92f5d0b72b5f1ba
Vlad <d_m_n04@mail.ru>
2022-06-12 13:08:50 +0300

    Add env

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php
 
 require_once 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-// ID ce0ac435-354b-4365-9c25-72a5fce90a38
-$payeerTradeApi = new Inc\Classes\PayeerTradeApi([
-    'secret' => 'YuHnoB3hwbAtccL8',
-    'id' => 'ce0ac435-354b-4365-9c25-72a5fce90a38',
-]);
+$secret = $_ENV['PAYEER_API_SECRET'] ?? '';
+$id = $_ENV['PAYEER_API_ID'] ?? '';
+
+$payeerTradeApi = new Inc\Classes\PayeerTradeApi($secret, $id);
 $pairs = ['BTC_USDT', 'BTC_RUB'];
 echo '<pre>';
 try {
 //    var_dump($payeerTradeApi->trades());
 //    var_dump($payeerTradeApi->info());
-//    var_dump($payeerTradeApi->checkConnection());
+    var_dump($payeerTradeApi->checkConnection());
 //    var_dump($payeerTradeApi->ticker());
-    var_dump($payeerTradeApi->orders());
+//    var_dump($payeerTradeApi->orders($pairs));
+//    var_dump($payeerTradeApi->myTrades());
+//    var_dump($payeerTradeApi->ordersCancel(action: 'buy'));
 } catch (\Exception $exception) {
+    var_dump('Error!');
     var_dump($exception->getMessage());
 }
